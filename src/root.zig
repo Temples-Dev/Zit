653b69d517369879d9534d265554e960e7e7d2ea
Temples-Dev <templelomotey@gmail.com>
2026-06-30 12:19:40 +0000

    refactor: update root.zig to export error and subcommand APIs

--- src/root.zig
+++ src/root.zig
@@ -1,18 +1,8 @@
-//! By convention, root.zig is the root source file when making a package.
-const std = @import("std");
-const Io = std.Io;
+//! Zit – public library API.
+pub const Oid = @import("object/oid.zig").Oid;
+pub const ObjectType = @import("object/object.zig").ObjectType;
+pub const Object = @import("object/object.zig").Object;
+pub const Store = @import("object/store.zig").Store;
+pub const Repo = @import("repo.zig").Repo;
+pub const errors = @import("errors.zig");
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
-}
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
