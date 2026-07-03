d5d19cca470e09d6dd7c61d3d90c39631a02a2d4
Temples-Dev <templelomotey@gmail.com>
2026-07-03 11:33:29 +0000

    refact: adapt root.zig to style guide rules (OID export)

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,7 @@
 //! Zit – public library API.
-pub const Oid = @import("object/oid.zig").Oid;
+pub const OID = @import("object/oid.zig").OID;
 pub const ObjectType = @import("object/object.zig").ObjectType;
 pub const Object = @import("object/object.zig").Object;
 pub const Store = @import("object/store.zig").Store;
 pub const Repo = @import("repo.zig").Repo;
 pub const errors = @import("errors.zig");
-
